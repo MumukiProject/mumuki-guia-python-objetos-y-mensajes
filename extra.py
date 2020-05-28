bb4eb87d370a3265f7ed47b4f433400843f55659
franco@mumuki.org <franco@mumuki.org>
2020-05-28 21:32:47 +0000

    Mumuki Export on 2020-05-28 21:32:47 +0000

--- extra.py
+++ extra.py
@@ -1,81 +1,82 @@
-module BuenosAires
+class BuenosAiresClass:
   def self.kilometr(self):
-    0
+    return 0
+
+
+BuenosAires = BuenosAiresClass()
+
+# module Iruya
+#   def self.kilometr(self):
+#     1710
 
 
 
-module Iruya
-  def self.kilometr(self):
-    1710
+# module Obera
+#   def self.kilometr(self):
+#     1040
 
 
 
-module Obera
-  def self.kilometr(self):
-    1040
+# module Mercedes
+#   def self.cantar!
+#     "♪ una voz antigua de viento y de sal ♫"
 
 
 
-module Mercedes
-  def self.cantar!
-    "♪ una voz antigua de viento y de sal ♫"
+# module Norita
+#   def self.cantar!
+#     'priiiip priiiip'
+
+
+#   def self.volar_en_circulos!
+
+
+#   def self.comer_lombriz!
 
 
 
-module Norita
-  def self.cantar!
-    'priiiip priiiip'
+# module Pepita
+#   self.energia = 100
+#   self.ciudad = Obera
+
+#   def self.energia=(self, una_energia):
+#     self.energia = una_energia
 
 
-  def self.volar_en_circulos!
+#   def self.energi(self):
+#     self.energia
 
 
-  def self.comer_lombriz!
+#   def self.ciuda(self):
+#     self.ciudad
 
 
-
-module Pepita
-  self.energia = 100
-  self.ciudad = Obera
-
-  def self.energia=(self, una_energia):
-    self.energia = una_energia
+#   def self.cantar!
+#     'pri pri pri'
 
 
-  def self.energi(self):
-    self.energia
+#   def self.comer_lombriz!
+#     self.energia += 20
+#     return
 
 
-  def self.ciuda(self):
-    self.ciudad
+#   def self.comer_alpiste!(self, una_energia):
+#     self.energia += una_energia * 15
+#     return
 
 
-  def self.cantar!
-    'pri pri pri'
+#   def self.volar_en_circulos!
+#     self.energia -= 10
+#     return
 
 
-  def self.comer_lombriz!
-    self.energia += 20
-    return
+#   def self.volar_hacia!(self, una_ciudad):
+#     self.energia -= self.distancia(una_ciudad) * 3
+#     self.ciudad = una_ciudad
+#     return
 
 
-  def self.comer_alpiste!(self, una_energia):
-    self.energia += una_energia * 15
-    return
-
-
-  def self.volar_en_circulos!
-    self.energia -= 10
-    return
-
-
-  def self.volar_hacia!(self, una_ciudad):
-    self.energia -= self.distancia(una_ciudad) * 3
-    self.ciudad = una_ciudad
-    return
-
-
-  def self.distancia(self, una_ciudad):
-    (self.ciudad.kilometro - una_ciudad.kilometro).abs
+#   def self.distancia(self, una_ciudad):
+#     (self.ciudad.kilometro - una_ciudad.kilometro).abs
 
 
