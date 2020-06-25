88313f1ea3a1da53a7d448fa156f5d951e9bfc81
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-06-25 21:31:24 +0000

    Mumuki Export on 2020-06-25 21:31:24 +0000

--- 00014_Más argumentos/test.py
+++ 00014_Más argumentos/test.py
@@ -1,4 +1,5 @@
-class tests(unittest):
+
+
   def test_haber_perdido_energia(self):
     """haber perdido energía"""
     assertEqual(Pepita.energia,3580)
@@ -6,6 +7,3 @@ class tests(unittest):
   def test_estar_en_Obera(self):
     """estar en Oberá"""
     assertTrue(isInstance(Pepita.ciudad,Obera))
-    
-    
-    
