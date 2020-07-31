f3156b64d5a324db9e39f97af02385671ae59ec3
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-07-31 21:20:16 +0000

    Mumuki Export on 2020-07-31 21:20:16 +0000

--- 00010_Tu primer programa con objetos/test.py
+++ 00010_Tu primer programa con objetos/test.py
@@ -1,8 +1,10 @@
-
+#...content...#
 
 class Test(unittest.TestCase):
   def test_inspect(self):
+    dir()
     self.assertTrue(False,dir(self))
+    
     
   def test_inspect2(self):
     self.assertTrue(False,locals())
