707e986fa530293e4b4eed361c647fed66634afb
Leandro Batlle <leandro.batlle@gmail.com>
2020-08-07 17:14:08 -0300

    Revert "Mumuki Export on 2020-07-31 21:20:16 +0000"
    This reverts commit f3156b64d5a324db9e39f97af02385671ae59ec3.

--- 00010_Tu primer programa con objetos/test.py
+++ 00010_Tu primer programa con objetos/test.py
@@ -1,10 +1,8 @@
-#...content...#
+
 
 class Test(unittest.TestCase):
   def test_inspect(self):
-    dir()
     self.assertTrue(False,dir(self))
-    
     
   def test_inspect2(self):
     self.assertTrue(False,locals())
