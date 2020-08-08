33e149a2dfdfc0ee2b8e5f0be2e923dcea30e1ad
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-08-08 11:47:01 +0000

    Mumuki Export on 2020-08-08 11:47:01 +0000

--- 00010_Tu primer programa con objetos/test.py
+++ 00010_Tu primer programa con objetos/test.py
@@ -1,4 +1,4 @@
 class Test(unittest.TestCase):
     def test_Pepita_quedó_con_energia_correcta(self):
         result = Pepita.energia
-        self.assertEqual(result, 150,"El mensaje `Pepita.energia` devolvió {} y debería devolver 150. En otras: palabras el atributo `energía` de Pepita no tiene el valor pedido".format(result))
+        self.assertEqual(result, 150,"El mensaje `Pepita.energia` devolvió {} y debería devolver 150.\n En otras: palabras el atributo `energía` de Pepita no tiene el valor pedido".format(result))
