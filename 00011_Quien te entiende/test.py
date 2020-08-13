0f0b0fe8725dd208c53b77e3637383c8a42860f4
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-08-13 23:49:33 +0000

    Mumuki Export on 2020-08-13 23:49:33 +0000

--- 00011_Quien te entiende/test.py
+++ 00011_Quien te entiende/test.py
@@ -11,8 +11,13 @@
 #     expect(interfaz_norita).to match_array ['cantar!', 'comer_lombriz!', 'volar_en_circulos!']
 class Test(unittest.TestCase):
 
-  def test_interfaz_de_Norita(self):
-    interfaz_esperada=["cantar()","comer_lombriz()","energia","volar_en_circulos()"]
+  def test_interfaz_de_Norita_tiene_todos_los_mensajes(self):
+    interfaz_esperada=["energia","cantar()","comer_lombriz()","volar_en_circulos()"]
     interfaz_entregada=interfaz_norita
     
-    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz de {nombre} es algo como {iface}".format(nombre="Mercedes", iface=str(interfaz_esperada)))
+    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz de {nombre} es algo como {iface}".format(nombre="Norita", iface=str(interfaz_esperada[:2])))
+
+  def test_interfaz_de_Norita_con_minusculas(self):
+    interfaz_entregada=interfaz_norita
+    todos_minusculas=all([str(x).islower() for x in interfaz_entregada])
+    self.assertTrue(todos_minusculas,"la interfaz de {nombre} tiene todas palabras en minúsculas (que pueden estar unidas por espacios subrayados), es algo como {iface}".format(nombre="Norita", iface=str(interfaz_esperada[:2])))
