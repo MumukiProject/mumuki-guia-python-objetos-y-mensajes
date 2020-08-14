5b65f45edc7182adebe588f6d0ef6d1e9eedbb3c
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-08-14 00:23:33 +0000

    Mumuki Export on 2020-08-14 00:23:33 +0000

--- 00011_Quien te entiende/test.py
+++ 00011_Quien te entiende/test.py
@@ -11,6 +11,7 @@
 #     expect(interfaz_norita).to match_array ['cantar!', 'comer_lombriz!', 'volar_en_circulos!']
 
 mayusminus="para Python, 'A' no es el mismo que 'a'."
+interfaz_ejemplo="['mensaje1','mensaje2()']"
 
 
 class Test(unittest.TestCase):
@@ -19,31 +20,31 @@ class Test(unittest.TestCase):
     interfaz_esperada=["cantar()","comer_lombriz()","volar_en_circulos()"]
     interfaz_entregada=interfaz_norita
     
-    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz de {nombre} es algo como {iface}".format(nombre="Norita", iface=str(interfaz_esperada[:2])))
+    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz de {nombre} es algo como {iface}".format(nombre="Norita", iface=interfaz_ejemplo))
 
   def test_interfaz_de_Norita_con_palabras_en_minusculas(self):
     interfaz_esperada=["cantar()","comer_lombriz()","volar_en_circulos()"]    
     interfaz_entregada=interfaz_norita
     todos_minusculas=all([str(x).islower() for x in interfaz_entregada])
-    self.assertTrue(todos_minusculas,mayusminus+" La interfaz de {nombre} tiene todas palabras en minúsculas (que pueden estar unidas por espacios subrayados), es algo como {iface}".format(nombre="Norita", iface=str(interfaz_esperada[:2])))
+    self.assertTrue(todos_minusculas,mayusminus+" La interfaz de {nombre} tiene todas palabras en minúsculas (que pueden estar unidas por espacios subrayados), es algo como {iface}".format(nombre="Norita", iface=interfaz_ejemplo))
 
   def test_interfaz_de_Mercedes_tiene_todos_los_mensajes_que_entiende(self):
     interfaz_esperada=["energia","cantar()","comer_lombriz()","volar_en_circulos()"]
     interfaz_entregada=interfaz_mercedes
     
-    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz de {nombre} es algo como {iface}".format(nombre="Mercedes", iface=str(interfaz_esperada[:2])))
+    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz de {nombre} es algo como {iface}".format(nombre="Mercedes", iface=interfaz_ejemplo))
 
   def test_interfaz_de_Mercedes_con_palabras_en_minusculas(self):
     interfaz_entregada=interfaz_mercedes
     todos_minusculas=all([str(x).islower() for x in interfaz_entregada])
-    self.assertTrue(todos_minusculas,mayusminus+" La interfaz de {nombre} tiene todas palabras en minúsculas (que pueden estar unidas por espacios subrayados), es algo como {iface}".format(nombre="Mercedes", iface=str(interfaz_esperada[:2])))
+    self.assertTrue(todos_minusculas,mayusminus+" La interfaz de {nombre} tiene todas palabras en minúsculas (que pueden estar unidas por espacios subrayados), es algo como {iface}".format(nombre="Mercedes", iface=interfaz_ejemplo)))
 
 
   def test_interfaz_de_Mercedes_tiene_ambos_tipos_de_mensaje(self):
     interfaz_entregada=interfaz_mercedes
     ambos_tipos=any([not(str(x).endswith("()")) for x in interfaz_entregada])
     ambos_tipos&=any([str(x).endswith("()") for x in interfaz_entregada])
-    self.assertTrue(todos_minusculas,"la interfaz de {nombre} tiene todas palabras en minúsculas (que pueden estar unidas por espacios subrayados), es algo como {iface}".format(nombre="Mercedes", iface=str(interfaz_esperada[:2])))
+    self.assertTrue(todos_minusculas,"la interfaz de {nombre} tiene todas palabras en minúsculas (que pueden estar unidas por espacios subrayados), es algo como {iface}".format(nombre="Mercedes", iface=interfaz_ejemplo)))
     
 
     
