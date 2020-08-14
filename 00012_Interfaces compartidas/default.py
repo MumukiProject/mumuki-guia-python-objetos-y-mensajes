cb3429c2ee72374e4d61856c1415b2cd2a79f393
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-08-14 02:05:12 +0000

    Mumuki Export on 2020-08-14 02:05:12 +0000

--- 00012_Interfaces compartidas/default.py
+++ 00012_Interfaces compartidas/default.py
@@ -11,7 +11,7 @@ interfaz_compartida_entre_mercedes_y_norita = [
 # ¿Qué interfaz comparten Pepita y Norita?
 interfaz_compartida_entre_pepita_y_norita = [
   #...completar acá... 
-]
+  ]
 
 # ¿Qué interfaz comparten Mercedes, Norita y Pepita?
 interfaz_compartida_entre_todas = [
