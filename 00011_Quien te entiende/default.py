324776a5697f97182fc0223460f0d5231fcd6da5
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-08-14 02:10:41 +0000

    Mumuki Export on 2020-08-14 02:10:41 +0000

--- 00011_Quien te entiende/default.py
+++ 00011_Quien te entiende/default.py
@@ -8,10 +8,10 @@ interfaz_norita = [
   #... completar acá ... 
   #... recordando las comillas 
   #... y las comas al final...
-  #..."mensaje1",
-  #..."mensaje2()",
-  #..."mensaje3()",
-]
+  "mensaje1",
+  "mensaje2()",
+  "mensaje3()",
+] #la lista termina con este corchete 
   
 interfaz_mercedes = [
   #...completar acá...
