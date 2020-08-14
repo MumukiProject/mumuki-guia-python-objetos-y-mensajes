50cea6ae1d7c8c55110156de06273f5f914bfb21
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-08-14 02:19:58 +0000

    Mumuki Export on 2020-08-14 02:19:58 +0000

--- 00012_Interfaces compartidas/test.py
+++ 00012_Interfaces compartidas/test.py
@@ -9,13 +9,27 @@
 #
 #   it 'todas':
 #     expect(interfaz_compartida_entre_todas).to match_array ['cantar!']
+interfaz_ejemplo="['mensaje1','mensaje2()']"
+
 
 
 class Test(unittest.TestCase):
 
   def test_interfaz_compartida_entre_mercedes_y_norita(self):
-    interfaz_esperada=["cantar()","comer_lombriz()","volar_en_circulos()"]
-    interfaz_entregada=interfaz_norita
+    interfaz_esperada=["cantar()"]
+    interfaz_entregada=interfaz_compartida_entre_mercedes_y_norita 
     
-    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz de {nombre} es algo como {iface}".format(nombre="Norita", iface=interfaz_ejemplo))
+    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz compartida entre {nombre} es algo como {iface}".format(nombre="Mercedes y Norita", iface=interfaz_ejemplo))
 
+
+  def test_interfaz_compartida_entre_todas(self):
+    interfaz_esperada=["cantar()"]
+    interfaz_entregada=interfaz_compartida_entre_todas
+    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz compartida entre {nombre} es algo como {iface}".format(nombre="todos los objetos", iface=interfaz_ejemplo))
+
+
+  def test_interfaz_compartida_entre_pepita_y_norita(self):
+    interfaz_esperada=["cantar()","comer_lombriz()","volar_en_circulos()"]
+    interfaz_entregada=interfaz_compartida_entre_pepita_y_norita
+    
+    self.assertEqual(set(interfaz_esperada),set(interfaz_entregada),"la interfaz compartida entre {nombre} es algo como {iface}".format(nombre="Pepita y Norita", iface=interfaz_ejemplo))
